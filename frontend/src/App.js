ba2c9ceaae7eca4afce342f009fceda34f562313
shamilv05 <vagabov.sha@phystech.edu>
2019-11-05 23:18:28 +0300

    button component added to react server

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './App.css'
+import { connect, sendMessage } from './api/index';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(props) {
+        super(props);
+        connect();
+    }
+
+    sendMessage() {
+        sendMessage('Hello, Shamil');
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <button onClick={this.sendMessage}>Send</button>
+            </div>
+        )
+    }
 }
 
 export default App;
--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,4 +1,4 @@
-let socket = new WebSocket("ws://localhost:8080/connection");
+let socket = new WebSocket("ws://127.0.0.1:8080/connection");
 
 let connect = () => {
     console.log("Attempt...");
