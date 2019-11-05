19db3f9776eda60c6f3215a0008da4458285e952
shamilv05 <vagabov.sha@phystech.edu>
2019-11-05 23:01:25 +0300

    websocket added

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gorilla/websocket"
+	"log"
+	"net/http"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize: 1024,
+	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+func reader(conn *websocket.Conn) {
+	for {
+		messageType, p, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+		}
+
+		fmt.Println(string(p))
+
+		if err := conn.WriteMessage(messageType, p); err != nil {
+			log.Println(err)
+			return
+		}
+	}
+}
+
+func serveWebSocket(w http.ResponseWriter, r *http.Request) {
+	websock, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+	}
+	reader(websock)
+}
+
+func setupRoutes() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Simple Server")
+	})
+	http.HandleFunc("/connection", serveWebSocket)
+}
 
 func main() {
-	fmt.Println("Hello World")
+	setupRoutes()
+	http.ListenAndServe("127.0.0.1:8080", nil)
 }
---
+++ frontend/src/api/index.js
@@ -0,0 +1,28 @@
+let socket = new WebSocket("ws://localhost:8080/connection");
+
+let connect = () => {
+    console.log("Attempt...");
+
+    socket.onopen = () => {
+        console.log("Connected");
+    };
+
+    socket.onmessage = (msg) => {
+        console.log(msg);
+    };
+
+    socket.onclose = (e) => {
+        console.log("Socket Closed: ", e);
+    }
+
+    socket.onerror = (err) => {
+        console.log("Socket Error: ", err);
+    }
+};
+
+let sendMessage = (msg) => {
+    console.log("Sending msg: ", msg);
+    socket.send(msg);
+};
+
+export { connect, sendMessage };
