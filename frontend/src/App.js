f0fd9db67e12f6ec18b0fc2d9a4f85b0d06c699d
shamilv05 <vagabov.sha@phystech.edu>
2019-11-06 20:23:56 +0300

    History component added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css'
 import { connect, sendMessage } from './api/index';
 import Header from './components/Header/Header'
+import History from "./components/History/index";
 
 class App extends Component {
     constructor(props) {
@@ -17,6 +18,7 @@ class App extends Component {
         return (
             <div className="App">
                 <Header />
+                <History />
                 {/*<button onClick={this.sendMessage}>Send</button>*/}
             </div>
         )
---
+++ frontend/src/components/History/History.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './History.scss';
+
+class History extends Component {
+    render() {
+        return (
+            <div className="history">
+                <h2>History of chats:</h2>
+            </div>
+        );
+    }
+}
+
+export default History;
